eb869d447a918f92cf913d6b180853df69182388
bls <blair.strang@covata.com>
2014-11-19 18:46:48 +1100

    Add stats panel, fix bugs in feature checks.

--- creature.js
+++ creature.js
@@ -10,14 +10,14 @@ function arg_with_default(arg, def) {
 }
 
 // Constructs a creature.
-function Creature(props, lovesList, hatesList) {
+function Creature(props, lovesFeatures, hatesFeatures) {
     this.id = creatureID();
     this.bodyShape = props.bodyShape;
     this.bodyColour = props.bodyColour;
     this.eyeShape = props.eyeShape;
     this.lipShape = props.lipShape;
-    this.lovesList = arg_with_default(lovesList, this.features());
-    this.hatesList = arg_with_default(hatesList, this.notFeatures());
+    this.lovesFeatures = arg_with_default(lovesFeatures, this.features());
+    this.hatesFeatures = arg_with_default(hatesFeatures, this.notFeatures());
 }
 
 // Set constructor
@@ -50,14 +50,28 @@ Creature.prototype.notFeatures = function() {
     return _.difference(Creature.ALL_FEATURES, this.features());
 };
 
+// Set it so that this creature loves this feature. If they hated it before,
+// they don't anymore.
+Creature.prototype.addLoves = function(featureList) {
+    this.hatesFeatures = _.difference(this.hatesFeatures, featureList);
+    this.lovesFeatures = _.union(this.lovesFeatures, featureList);
+};
+
+// Set it so that this creature hates this feature. If they loved it before,
+// they don't anymore. Tough luck, Romeo.
+Creature.prototype.addHates = function(featureList) {
+    this.lovesFeatures = _.difference(this.lovesFeatures, featureList);
+    this.hatesFeatures = _.union(this.hatesFeatures, featureList);
+};
+
 // Check compatibility with otherCreature. Returns one of ['hate', 'love', 'meh'].
 Creature.prototype.compatibility = function(otherCreature) {
     // If otherCreature has any feature I hate, then I hate them.
-    if(!_.isEmpty(_.intersection(this.hatesList, otherCreature.features()))) {
+    if(!_.isEmpty(_.intersection(this.hatesFeatures, otherCreature.features()))) {
         return 'hate';
     }
     // If otherCreature has any feature I love, then I love them.
-    if(!_.isEmtpy(_.intersection(this.lovesList, otherCreature.features()))) {
+    if(!_.isEmpty(_.intersection(this.lovesFeatures, otherCreature.features()))) {
         return 'love';
     }
     // Otherwise... I'm meh.
